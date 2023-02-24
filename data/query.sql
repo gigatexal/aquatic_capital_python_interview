5e361aa3f2aef6ea812fcd660bad26e3a098f865
alex narayan <alex@alexandarnarayan.com>
2023-02-24 20:48:02 +0100

    format query

--- data/query.sql
+++ data/query.sql
@@ -1,16 +1,12 @@
-explain analyze select
-	--row_number() over (partition by "Station Name", cast("Measurement Timestamp" as date) order by "Measurement Timestamp" desc) as row_num
+SELECT
 	"Station Name"
-    ,cast("Measurement Timestamp" as DATE) as "Measurement Day"
-    ,min("Air Temperature") OVER (partition by "Station Name" ,CAST("Measurement Timestamp" AS DATE)) as min_temp
-  	,max("Air Temperature") OVER (partition by "Station Name" ,CAST("Measurement Timestamp" AS DATE)) as max_temp
-	,first_value("Air Temperature") OVER (partition by  "Station Name",cast("Measurement Timestamp" as date) ORDER BY "Measurement Timestamp" ASC) as first_temp
+    ,CAST("Measurement Timestamp" AS DATE) AS "Measurement Day"
+    ,MIN("Air Temperature") OVER (PARTITION BY "Station Name" ,CAST("Measurement Timestamp" AS DATE)) AS min_temp
+  	,MAX("Air Temperature") OVER (PARTITION BY "Station Name" ,CAST("Measurement Timestamp" AS DATE)) AS max_temp
+	,FIRST_VALUE("Air Temperature") OVER (PARTITION BY  "Station Name",cast("Measurement Timestamp" AS date) ORDER BY "Measurement Timestamp" ASC) AS first_temp
 	/* for the life of me I can't figure out why the last_value function doesn't work list the first_value function ... using first_value and reverse order */
-	,first_value("Air Temperature") OVER (partition by  "Station Name", cast("Measurement Timestamp" as date) ORDER BY "Measurement Timestamp" DESC) as last_temp
-from
+	,FIRST_VALUE("Air Temperature") OVER (PARTITION BY  "Station Name", cast("Measurement Timestamp" AS date) ORDER BY "Measurement Timestamp" DESC) AS last_temp
+FROM
 	weather
-	qualify row_number() over (partition by "Station Name", cast("Measurement Timestamp" as date) order by "Measurement Timestamp" desc) = 1
-order by 2 asc,1
-	
-
---create index ix_foo on weather ("Station Name", "Measurement Timestamp", "Air Temperature");
+	QUALIFY ROW_NUMBER() OVER (PARTITION BY "Station Name", CAST("Measurement Timestamp" AS DATE) ORDER BY "Measurement Timestamp" DESC) = 1
+ORDER BY 2 ASC,1
