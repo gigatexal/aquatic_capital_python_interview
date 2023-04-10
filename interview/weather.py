44fecb96e93edf2fcb656f099893b5ce1682fb50
alex narayan <alex@alexandarnarayan.com>
2023-04-10 18:09:44 +0200

    remove added file

--- interview/weather.py
+++ interview/weather.py
@@ -1,3 +1,13 @@
+import pandas as pd
+import datetime
+from typing import TextIO
 
-def process_csv(reader, writer):
-    writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
+def process_csv(reader: TextIO, writer: TextIO):
+    with reader as r:
+        weather_data = pd.read_csv(r, parse_dates=['Measurement Timestamp'])
+        # Station Name
+        # Measurement Timestamp - convert to date
+        # Air Temperature
+        d: pd.DataFrame = weather_data[['Station Name', 'Measurement Timestamp', 'Air Temperature']]
+        d1=d.assign(date=lambda x: x["Measurement Timestamp"].dt.date)
+        print(d1)
